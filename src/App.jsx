c2bb11f45bb0517f6cd6339bcd79992a633bdf84
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-05-04 17:10:25 +0100

    Add Header component to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import heroImage from "./Images/hero.png";
 import "./App.css";
+import Header from "./Components/Header";
 
 export default function App() {
   return (
     <>
       <div className="header-hero-background-image">
         <img src={heroImage} alt="Background Image" />
+        <Header />
       </div>
     </>
   );
