9afd43f98eff440e9807261703ad4ac99815b317
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-05-04 16:37:05 +0100

    Add header and hero background image in the App.jsx file and decorate in the App.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import heroImage from "./Images/hero.png";
 import "./App.css";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <div className="header-hero-background-image">
+        <img src={heroImage} alt="Background Image" />
+      </div>
+    </>
+  );
 }
