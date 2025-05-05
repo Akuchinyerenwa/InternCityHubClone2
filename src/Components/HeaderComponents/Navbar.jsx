f245311333ca1b3038b2477c94544fd78bbadf9d
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-05-05 15:59:43 +0100

    Add all the Header media query decoration on all the Header component files

--- src/Components/HeaderComponents/Navbar.jsx
+++ src/Components/HeaderComponents/Navbar.jsx
@@ -1,15 +1,16 @@
 import "./Navbar.css";
-import LinkButton from "./NavbarComponents/LinkButton";
+import { HamburgerCheck } from "./NavbarComponents/NavbarList";
 import NavbarList from "./NavbarComponents/NavbarList";
+import LinkButton from "./NavbarComponents/LinkButton";
+import { Hamburger } from "./NavbarComponents/NavbarList";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <HamburgerCheck />
       <NavbarList />
       <LinkButton content="Start Free Trial" />
-      <div className="hamburger">
-        <div className="hamburger-bar"></div>
-      </div>
+      <Hamburger />
     </nav>
   );
 };
--- src/Components/HeaderComponents/NavbarComponents/NavbarList.jsx
+++ src/Components/HeaderComponents/NavbarComponents/NavbarList.jsx
@@ -1,6 +1,10 @@
 import "./NavbarList.css";
 import NavbarListItem from "./NavbarListComponents/NavbarListItem";
 
+const HamburgerCheck = () => {
+  return <input type="checkbox" name="check" id="check" className="check" />;
+};
+
 const NavbarList = () => {
   const listItem = ["English", "Naira(N)", "About", "Pricing", "Sign in"];
 
@@ -11,4 +15,14 @@ const NavbarList = () => {
   return <ul className="navbar-list">{listItemMap}</ul>;
 };
 
+const Hamburger = () => {
+  return (
+    <div className="hamburger">
+      <div className="hamburger-bar"></div>
+    </div>
+  );
+};
+
+export { HamburgerCheck };
 export default NavbarList;
+export { Hamburger };
