ad76fdcfd16d7f70784ae5cd4efe1d7a1a92e5d0
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-05-07 10:03:06 +0100

    Add all the Features components and final setup on the Features.jsx file

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,10 +1,26 @@
 import "./Features.css";
 import FeaturesContents from "./FeaturesComponents/FeaturesContents";
+import FeaturesArrayOfObjects from "./FeaturesComponents/FeaturesContent.js";
 
 const Features = () => {
+  const FeaturesObjectsContents = FeaturesArrayOfObjects.map((object) => {
+    return (
+      <FeaturesContents
+        key={object.id}
+        image={object.image.src}
+        alt={object.image.alt}
+        title={object.title}
+        description={object.description}
+      />
+    );
+  });
+
   return (
     <div className="features-root">
-      <FeaturesContents />
+      <h4>Features</h4>
+      <div className="features-section-container">
+        {FeaturesObjectsContents}
+      </div>
     </div>
   );
 };
