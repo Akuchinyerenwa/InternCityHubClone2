6dce3c0808faaf835f2d819a8dff38ad0485a76b
Akuchinyerenwa OPUS <littlepetero@gmail.com>
2025-05-07 09:24:02 +0100

    Create and setup FeaturesContent.js file

---
+++ src/Components/FeaturesComponents/FeaturesContent.js
@@ -0,0 +1,36 @@
+import Invoice from "./Images/invoice.png";
+import Notes from "./Images/notes.png";
+import Skills from "./Images/skills.png";
+
+export default [
+  {
+    id: 1,
+    image: {
+      src: Invoice,
+      alt: "Invoice image",
+    },
+    title: "Lorem Management",
+    description:
+      "Vitae aperiam quam quo quaerat facere, nisi recusandae esse quis officiis eveniet",
+  },
+  {
+    id: 2,
+    image: {
+      src: Notes,
+      alt: "Notes image",
+    },
+    title: "Lorem Request",
+    description:
+      "Vitae aperiam quam quo quaerat facere, nisi recusandae esse quis officiis eveniet",
+  },
+  {
+    id: 3,
+    image: {
+      src: Skills,
+      alt: "Skills image",
+    },
+    title: "Lorem Tracking",
+    description:
+      "Vitae aperiam quam quo quaerat facere, nisi recusandae esse quis officiis eveniet",
+  },
+];
